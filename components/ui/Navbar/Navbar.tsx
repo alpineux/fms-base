938870653475ac50910105a4d7d32ba1b5af03bb
alpineux <99145815+alpineux@users.noreply.github.com>
2023-06-19 17:28:40 -0600

    installed encoding

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -13,50 +13,52 @@ export default async function Navbar() {
   } = await supabase.auth.getUser();
 
   return (
-    <nav className={s.root}>
-      <a href="#skip" className="sr-only focus:not-sr-only">
-        Skip to content
-      </a>
-      <div className="max-w-6xl px-6 mx-auto">
-        <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
-          <div className="flex items-center flex-1">
-            <Link href="/" className={s.logo}>
-              fms
-            </Link>
-            <nav className="hidden ml-6 space-x-2 lg:block">
-              
-              {!user ?
-                <>
-                  <Link href="/" className={s.link}>
-                    pricing
-                  </Link>
-                </>
-              :
-                <>
-                  <Link href="/account" className={s.link}>
-                    account
-                  </Link>
-                  <Link href="/account" className={s.link}>
-                    premium
-                  </Link>
-                  <Link href="/admin" className={s.link}>
-                    admin
-                  </Link>
-                </>
-              }
-            </nav>
-          </div>
-          <div className="flex justify-end flex-1 space-x-8">
-            {user ? (
-              <SignOutButton />
-            ) : (
-              <Link href="/signin" className={s.link}>
-                sign in
+    <>
+      <nav className={s.root}>
+        <a href="#skip" className="sr-only focus:not-sr-only">
+          Skip to content
+        </a>
+        <div className="max-w-6xl px-6 mx-auto">
+          <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
+            <div className="flex items-center flex-1">
+              <Link href="/" className={s.logo}>
+                fms
               </Link>
-            )}
+              <nav className="hidden ml-6 space-x-2 lg:block">
+                
+                {!user ?
+                  <>
+                    <Link href="/" className={s.link}>
+                      pricing
+                    </Link>
+                  </>
+                :
+                  <>
+                    <Link href="/account" className={s.link}>
+                      account
+                    </Link>
+                    <Link href="/account" className={s.link}>
+                      premium
+                    </Link>
+                    <Link href="/admin" className={s.link}>
+                      admin
+                    </Link>
+                  </>
+                }
+              </nav>
+            </div>
+            <div className="flex justify-end flex-1 space-x-8">
+              {user ? (
+                <SignOutButton />
+              ) : (
+                <Link href="/signin" className={s.link}>
+                  sign in
+                </Link>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
